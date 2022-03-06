00400cf0f11b4781d805e9422511e66ea2592d1a
nanimation2 <33375515+nanimation2@users.noreply.github.com>
2022-03-06 14:25:35 -0600

    Add files via upload

---
+++ frontEnd/Semana 2 practicas/img/Spikes Pattern Texture - Free Subtle Patterns _ Toptal®_files/_ssgManifest.js
@@ -0,0 +1,1 @@
+self.__SSG_MANIFEST=new Set(["\u002Fpatterns","\u002F","\u002Fsubmit","\u002Ftag\u002Fall","\u002F[slug]","\u002Fpage\u002F[page]","\u002Ftag\u002F[tag]","\u002Ftag\u002F[tag]\u002Fpage\u002F[page]"]);self.__SSG_MANIFEST_CB&&self.__SSG_MANIFEST_CB()
